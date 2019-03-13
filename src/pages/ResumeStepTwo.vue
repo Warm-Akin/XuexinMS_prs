8d48cd0255702986a21433e4d4e834bfc05c32d5
Warm-Akin <1028825411@qq.com>
2019-03-13 23:52:20 +0800

    change the cropper component's method logic.

--- src/pages/ResumeStepTwo.vue
+++ src/pages/ResumeStepTwo.vue
@@ -13,50 +13,53 @@
             <el-row class="content-title">上传简历照片</el-row>
             <el-row>&nbsp;</el-row>
             <!--<el-row>-->
-              <!--<el-upload-->
-                <!--class="avatar-uploader"-->
-                <!--:action="uploadPhotoUrl"-->
-                <!--:show-file-list="false"-->
-                <!--:on-success="handleAvatarSuccess"-->
-                <!--:before-upload="beforeAvatarUpload">-->
-                <!--<img v-if="imageUrl" :src="imageUrl" class="avatar">-->
-                <!--<i v-else class="el-icon-plus avatar-uploader-icon"></i>-->
-                <!--<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过5MB</div>-->
-              <!--</el-upload>-->
+            <!--<el-upload-->
+            <!--class="avatar-uploader"-->
+            <!--:action="uploadPhotoUrl"-->
+            <!--:show-file-list="false"-->
+            <!--:on-success="handleAvatarSuccess"-->
+            <!--:before-upload="beforeAvatarUpload">-->
+            <!--<img v-if="imageUrl" :src="imageUrl" class="avatar">-->
+            <!--<i v-else class="el-icon-plus avatar-uploader-icon"></i>-->
+            <!--<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过5MB</div>-->
+            <!--</el-upload>-->
             <!--</el-row>-->
             <el-row>
               <el-button type="primary" @click="changeClick">选择图片</el-button>
-              <input type="file" id="true-button" accept="image/png, image/jpeg, image/jpg" @change="uploadImg($event,1)" />
+              <input type="file" id="true-button" accept="image/png, image/jpeg, image/jpg"
+                     @change="uploadImg($event,1)"/>
             </el-row>
             <el-row class="cropper-content">
-              <!--<div class="cropper-content" style="margin-top:-60px;margin-left:260px;">-->
-                <div class="cropper">
-                  <vueCropper
-                    ref="cropper"
-                    :img="option.img"
-                    :outputSize="option.size"
-                    :outputType="option.outputType"
-                    :info="true"
-                    :full="option.full"
-                    :canMove="option.canMove"
-                    :canMoveBox="option.canMoveBox"
-                    :original="option.original"
-                    :autoCrop="option.autoCrop"
-                    :autoCropWidth="option.autoCropWidth"
-                    :autoCropHeight="option.autoCropHeight"
-                    :fixedBox="option.fixedBox"
-                    @realTime="realTime"
-                    @imgLoad="imgLoad"></vueCropper>
-                </div>
-                <div style="margin-left: 22%;">
-                  <div class="show-preview"
-                       :style="{'width': '150px', 'height':'155px',  'overflow': 'hidden', 'margin': '5px'}">
-                    <div :style="previews.div" class="preview">
-                      <img :src="previews.url" :style="previews.img">
-                    </div>
+              <div class="cropper">
+                <vueCropper
+                  ref="cropper"
+                  :img="imageOption.img"
+                  :outputSize="imageOption.size"
+                  :outputType="imageOption.outputType"
+                  :info="true"
+                  :full="imageOption.full"
+                  :canMove="imageOption.canMove"
+                  :canMoveBox="imageOption.canMoveBox"
+                  :original="imageOption.original"
+                  :autoCrop="imageOption.autoCrop"
+                  :autoCropWidth="imageOption.autoCropWidth"
+                  :autoCropHeight="imageOption.autoCropHeight"
+                  :fixedBox="imageOption.fixedBox"
+                  @realTime="realTime"
+                  @imgLoad="imgLoad"></vueCropper>
+              </div>
+              <div style="margin-left: 22%;">
+                <div class="show-preview">
+                  <div :style="previews.div" class="preview">
+                    <img :src="previews.url" :style="previews.img">
                   </div>
                 </div>
-              <!--</div>-->
+              </div>
+            </el-row>
+            <el-row v-if="imageOption.img !== ''">
+              <el-col :span="8" class="upload-button">
+                <el-button type="primary" @click="uploadImage">上传</el-button>
+              </el-col>
             </el-row>
           </el-col>
         </el-col>
@@ -71,6 +74,7 @@
   import Menu from '@/components/ResumeMenu.vue';
   import Constant from '@/utils/Constant';
   import { getUserResumeInfo } from '@/service/student.service';
+  import axios from 'axios';
 
   export default {
     components: {
@@ -99,29 +103,29 @@
           threeInfo: ''
         },
         uploadPhotoUrl: '',
-        imageUrl: '',
+        // imageUrl: '',
         // todo
-        headImg: '',
+        // headImg: '',
         //剪切图片上传
-        crap: false,
+        // crap: false,
         previews: {},
-        option: {
+        imageOption: {
           img: '',
-          outputSize: 1, //剪切后的图片质量（0.1-1）
-          full: false, //输出原图比例截图 props名full
-          outputType: 'png',
+          outputSize: 1, // 剪切后的图片质量（0.1-1）
+          full: true, // 输出原图比例截图 props名full
+          outputType: 'png', // 剪切后图片类型
           canMove: true,
           original: false,
           canMoveBox: true,
           autoCrop: true,
-          autoCropWidth: 150,
-          autoCropHeight: 150,
-          fixedBox: false // 固定截图框大小
+          autoCropWidth: 160,
+          autoCropHeight: 230,
+          fixedBox: true // 固定截图框大小
         },
-        fileName: '',  //本机文件地址
-        downImg: '#',
-        imgFile: '',
-        uploadImgRealPath: '' //上传后的图片的地址（不带服务器域名）
+        fileName: '',
+        imageUrl: ''
+        // imgFile: '',
+        // uploadImgRealPath: '' //上传后的图片的地址（不带服务器域名）
       }
     },
     methods: {
@@ -137,8 +141,8 @@
         }
       },
       beforeAvatarUpload(file) {
-        const isCorrectType = ( file.type === 'image/png' || file.type === 'image/jpeg');
-        const isLt5M = file.size / (1024 * 1024 * 1024 * 1024 * 1024 ) < 5;
+        const isCorrectType = (file.type === 'image/png' || file.type === 'image/jpeg');
+        const isLt5M = file.size / (1024 * 1024 * 1024 * 1024 * 1024) < 5;
         if (!isCorrectType) {
           this.$message.error('上传头像图片仅支持 JPG/PNG 格式!');
         }
@@ -150,47 +154,69 @@
       async init() {
         let response = await getUserResumeInfo(this.userName);
         this.studentResume = response.data;
-        let blobObj = this.convertBase64UrlToBlob(this.studentResume.photoPath);
+        let photoPath = this.studentResume.photoPath;
+        this.initPhoto(photoPath);
+        // let blobObj = this.convertBase64UrlToBlob(this.studentResume.photoPath);
+        // this.imageUrl = URL.createObjectURL(blobObj);
+        // this.imageUrl = this.studentResume.photoPath;
+        // console.log('URL  ', this.imageUrl);
+      },
+      initPhoto(photoPath) {
+        let image = new Image();
+        image.crossOrigin = '';
+        image.src = photoPath;
+        let imageBase64 = this.getBase64Image(image);
+        console.log('base64==>', imageBase64);
+        let blobObj = this.convertBase64UrlToBlob(imageBase64);
         this.imageUrl = URL.createObjectURL(blobObj);
         // this.imageUrl = this.studentResume.photoPath;
         console.log('URL  ', this.imageUrl);
       },
-      convertBase64UrlToBlob(urlData){
-        var bytes = window.atob(urlData.split(',')[1]);        //去掉url的头，并转换为byte
+      convertBase64UrlToBlob(urlData) {
+        var bytes = window.atob(urlData.split(',')[1]);        //去掉url的头，并转换为byte
         //处理异常,将ascii码小于0的转换为大于0
-        var ab = new ArrayBuffer(bytes.length);
-        var ia = new Uint8Array(ab);
-        for (var i = 0; i < bytes.length; i++) {
-          ia[i] = bytes.charCodeAt(i);
+        var ab = new ArrayBuffer(bytes.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < bytes.length; i++) {
+          ia[i] = bytes.charCodeAt(i);
         }
-        return new Blob( [ab] , {type : 'image/jpeg'});
+        return new Blob([ab], {type: 'image/jpeg'});
+      },
+      getBase64Image(img) {
+        let canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        let ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
+        let dataURL = canvas.toDataURL("image/"+ext);
+        return dataURL;
       },
       // todo add
       uploadImg(e, num) {
+        // e => changeEvent, num => image's count
         console.log('uploadImg');
-        var _this = this;
-        //上传图片
-        var file = e.target.files[0]
-        this.beforeAvatarUpload(file);
-        // _this.fileName = file.name;
-        // if (!/\.(gif|jpg|jpeg|png|bmp|GIF|JPG|PNG)$/.test(e.target.value)) {
-        //   alert('图片类型必须是.gif,jpeg,jpg,png,bmp中的一种')
-        //   return false
-        // }
-        var reader = new FileReader();
+        let file = e.target.files[0];
+        let validFlag = this.beforeAvatarUpload(file);
+        if (!validFlag) {
+          this.imageOption.img = '';
+          return false;
+        }
+        this.fileName = file.name;
+        let reader = new FileReader();
         reader.onload = (e) => {
           let data;
           if (typeof e.target.result === 'object') {
             // 把Array Buffer转化为blob 如果是base64不需要
-            data = window.URL.createObjectURL(new Blob([e.target.result]))
+            data = window.URL.createObjectURL(new Blob([e.target.result]));
           }
           else {
-            data = e.target.result
+            data = e.target.result;
           }
           if (num === 1) {
-            _this.option.img = data
+            this.imageOption.img = data;
           } else if (num === 2) {
-            _this.example2.img = data
+            this.example2.img = data;
           }
         }
         // 转化为base64
@@ -200,11 +226,41 @@
       },
       // 实时预览函数
       realTime(data) {
+        console.log('preview');
+        console.log(data)
         this.previews = data
       },
       imgLoad(msg) {
-        console.log('imgLoad')
+        // 图片加载的回调 imgLoad ,返回结果 success / error
         console.log(msg)
+      },
+      //上传图片（点击上传按钮）
+      uploadImage() {
+        console.log('uploadImage')
+        let formData = new FormData();
+        this.$refs.cropper.getCropBlob((data) => {
+          // let img = window.URL.createObjectURL(data);
+          // this.model = true;
+          // this.modelSrc = img;
+          // 最后一个参数this.fileName给Blob对象取名
+          formData.append("file", data, this.fileName);
+          axios.post(this.uploadPhotoUrl, formData, {
+            contentType: false,
+            processData: false,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+          }).then((response) => {
+            console.log('upload===')
+            console.log(response);
+            let res = response.data;
+            if ((res.code === Constant.POPUP_EXCEPTION_CODE || res.code === 'ERROR') && res.msg !== '') {
+              this.$alert(res.msg, {
+                confirmButtonText: 'OK'
+              });
+            } else {
+              this.$message.success('图片上传成功');
+            }
+          });
+        });
       },
       changeClick() {
         document.getElementById('true-button').click();
@@ -217,8 +273,7 @@
     },
     mounted() {
       document.title = '步骤二';
-
-      // this.init();
+      this.init();
     }
   }
 </script>
@@ -285,7 +340,25 @@
     left: 20%;
   }
 
-  >>>.avatar-uploader .el-upload {
+  .upload-button {
+    position: relative;
+    display: flex;
+    display: -webkit-flex;
+    margin-top: 15px;
+    margin-left: 100px;
+    padding-right: 2%;
+    justify-content: flex-end;
+    -webkit-justify-content: flex-end;
+  }
+
+  .show-preview {
+    width: 160px;
+    height: 235px;
+    overflow: hidden;
+    margin: 5px;
+  }
+
+  >>> .avatar-uploader .el-upload {
     border: 1px dashed #d9d9d9;
     border-radius: 6px;
     cursor: pointer;
@@ -293,11 +366,11 @@
     overflow: hidden;
   }
 
-  >>>.avatar-uploader .el-upload:hover {
+  >>> .avatar-uploader .el-upload:hover {
     border-color: #409EFF;
   }
 
-  >>>.avatar-uploader-icon {
+  >>> .avatar-uploader-icon {
     font-size: 28px;
     color: #8c939d;
     width: 140px;
@@ -306,7 +379,7 @@
     text-align: center;
   }
 
-  >>>.avatar {
+  >>> .avatar {
     width: 140px;
     height: 160px;
     display: block;
